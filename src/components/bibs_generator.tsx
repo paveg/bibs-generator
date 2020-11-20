beb499aef9c1cb356c87c6bec98709cf935a9f4b
Ryota Ikezawa <pavegy@gmail.com>
2020-11-21 00:25:09 +0900

    :sparkles: Correspond fontColor and backgroundColor

--- src/components/bibs_generator.tsx
+++ src/components/bibs_generator.tsx
@@ -9,19 +9,12 @@ import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import ColorPicker from 'material-ui-color-picker';
 
 const useStyles = makeStyles((theme: Theme) => {
   const style = createStyles({
     root: {
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: 120,
-      },
-      '& .MuiButton-root': {
-        margin: theme.spacing(1),
-        width: 120,
-      },
-      '& .MuiSelect-root': {
+      '& .MuiButton-root, .MuiFormControl-root, .MuiSelect-root, .MuiFormControl-root': {
         margin: theme.spacing(1),
         width: 120,
       },
@@ -55,12 +48,28 @@ const getContext = (ref: MutableRefObject<any>): CanvasRenderingContext2D => {
 };
 
 const BibsGenerator: React.FC = () => {
-  const [font, setFont] = useState<string>('electricboots');
-  const canvasRef = useRef(null);
-
-  const saveCanvasRef = useRef(null);
   const classes = useStyles();
+
+  const [font, setFont] = useState<string>('electricboots');
   const [text, setText] = useState<string>('');
+  const [fontColor, setFontColor] = useState<string>('#252525');
+  const [backgroundColor, setBackgroundColor] = useState<string>('#ffffff');
+  const canvasRef = useRef(null);
+  const saveCanvasRef = useRef(null);
+
+  const configureContext = (ctx: CanvasRenderingContext2D, ref: any): CanvasRenderingContext2D => {
+    const canvas = ref.current;
+    ctx.lineJoin = 'round';
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.setTransform(1, 0, -0.18, 1, 0, 0);
+    ctx.fillStyle = fontColor;
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 0;
+    return ctx;
+  };
 
   const onInput = (event) => {
     setText(event.target.value);
@@ -73,18 +82,13 @@ const BibsGenerator: React.FC = () => {
   };
 
   useEffect(() => {
-    const canvas = canvasRef.current;
     const ctx: CanvasRenderingContext2D = getContext(canvasRef);
 
     ctx.font = `100px ${font}`;
-    ctx.lineJoin = 'round';
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.setTransform(1, 0, -0.18, 1, 0, 0);
-    ctx.strokeStyle = '#000';
-    ctx.lineWidth = 0;
+    configureContext(ctx, canvasRef);
     ctx.fillText(text, 70 + 4, 100 + 4);
-  }, [text, font]);
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+  }, [text, font, fontColor, backgroundColor]);
 
   const saveImage = () => {
     const canvas: any = saveCanvasRef.current;
@@ -92,12 +96,7 @@ const BibsGenerator: React.FC = () => {
     const info = saveInfo[font];
 
     ctx.font = `${info.pixel} ${font}`;
-    ctx.lineJoin = 'round';
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.setTransform(1, 0, -0.1, 1, 0, 0);
-    ctx.strokeStyle = '#000';
-    ctx.lineWidth = 0;
+    configureContext(ctx, saveCanvasRef);
     ctx.fillText(text, 324 + 4, 1500 + 4);
     const url = canvas.toDataURL('image/png', 1);
 
@@ -121,14 +120,28 @@ const BibsGenerator: React.FC = () => {
         <canvas width={400} height={150} ref={canvasRef} />
         <Divider />
       </Box>
-      <Box display="flex" justifyContent="center" className={classes.root}>
-        <Select
-          size="small"
-          labelId="font-label"
-          id="select-font"
-          value={font}
-          onChange={handleChange}
-        >
+      <Box display="flex" justifyContent="center" className={classes.root} m={2}>
+        <ColorPicker
+          name="font-color"
+          defaultValue={fontColor}
+          floatingLabelText="文字色"
+          value={fontColor}
+          onChange={(color) => {
+            setFontColor(color);
+          }}
+        />
+        <ColorPicker
+          name="background-color"
+          defaultValue={backgroundColor}
+          floatingLabelText="背景色"
+          value={backgroundColor}
+          onChange={(color) => {
+            setBackgroundColor(color);
+          }}
+        />
+      </Box>
+      <Box display="flex" justifyContent="center" className={classes.root} m={1}>
+        <Select labelId="font-label" id="select-font" value={font} onChange={handleChange}>
           {menuItems}
         </Select>
         <TextField
